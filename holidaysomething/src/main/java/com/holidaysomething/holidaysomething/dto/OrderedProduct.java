50617cd6fea87ed2506744f1ec14630d4d0c8dee
Gyumin Kim <starkying@gmail.com>
2018-12-07 11:58:42 +0900

    Add domain: shippings, shippings_companies, ordered_products, products, exchange_refund, exchange_refund_details

--- holidaysomething/src/main/java/com/holidaysomething/holidaysomething/dto/OrderedProduct.java
+++ holidaysomething/src/main/java/com/holidaysomething/holidaysomething/dto/OrderedProduct.java
@@ -3,15 +3,38 @@ package com.holidaysomething.holidaysomething.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ORDERED_PRODUCT")
 @Getter
 @Setter
 public class OrderedProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private int orderPrice;
+
+    @Column(nullable = false)
     private int savedMoney;
+
+    @Column(nullable = false)
     private int quantity;
-    private Long orderId;
-    private Long pruductId;
-    private Long exchangeRefundId;
+
+    @ManyToOne
+    @JoinColumn(name = "ordered_product_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "ordered_product_id")
+    private Long productId;
+
+    @OneToOne
+    @JoinColumn(name = "exchange_refund_id")
+    private ExchangeRefund exchangeRefund;
+
+
     private Long couponId;
 }
