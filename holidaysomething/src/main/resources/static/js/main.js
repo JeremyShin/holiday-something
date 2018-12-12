ce5b2b9635a756b66685677f3ad85ea196470675
Gyumin Kim <starkying@gmail.com>
2018-12-12 19:43:47 +0900

    Can't submit productOptionDelete button(admin) if all of the checkboxes aren't checked

--- holidaysomething/src/main/resources/static/js/main.js
+++ holidaysomething/src/main/resources/static/js/main.js
@@ -1,8 +1,26 @@
 let toggle = function(source) {
     let productOptionTrs = document.querySelectorAll('#productOptionTr');
     for (let i = 0; i < productOptionTrs.length; i++) {
-        let checkboxInput = productOptionTrs[i].getElementsByTagName('input')[0];
-        if (checkboxInput !== source)
-            checkboxInput.checked = source.checked;
+        let productOptionCheckboxInput = productOptionTrs[i].getElementsByTagName('input')[0];
+        if (productOptionCheckboxInput !== source)
+            productOptionCheckboxInput.checked = source.checked;
     }
 };
+
+let productOptionFormSubmitBtn = document.querySelector('#productOptionForm').querySelector('#productOptionFormSubmit');
+productOptionFormSubmitBtn.addEventListener('click', (event) => {
+    let productOptionCheckboxInput = document.getElementsByName('productOptionId');
+
+    // productOptionCheckboxInput이 하나도 체크되어 있지 않을 경우 submit 버튼 비활성화
+    let allBtnUnchecked = true;
+    for (let i = 0; i < productOptionCheckboxInput.length; i++) {
+        if (productOptionCheckboxInput[i].checked === true) {
+            allBtnUnchecked = false;
+            break;
+        }
+    }
+    if (allBtnUnchecked === true) {
+        event.preventDefault();
+        alert('적용할 옵션이 없습니다.');
+    }
+});
