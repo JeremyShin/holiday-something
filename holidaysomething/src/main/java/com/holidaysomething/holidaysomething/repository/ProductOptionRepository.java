aba6d5536ab8c882a639667b28459ffcd5e2b395
Gyumin Kim <starkying@gmail.com>
2018-12-13 12:34:02 +0900

    Delete unnessary comments

--- holidaysomething/src/main/java/com/holidaysomething/holidaysomething/repository/ProductOptionRepository.java
+++ holidaysomething/src/main/java/com/holidaysomething/holidaysomething/repository/ProductOptionRepository.java
@@ -10,11 +10,5 @@ import java.util.List;
 
 @Repository
 public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
-
-//    @Query(value="select me from Member me where me.loginId=(:loginId)")
-//    ProductOption findByIdContaining(@Param("loginId") String loginId);
-//
-//    List<ProductOption> findByOrderIdOrderByProductId(Long orderId);
-
-
+  
 }
