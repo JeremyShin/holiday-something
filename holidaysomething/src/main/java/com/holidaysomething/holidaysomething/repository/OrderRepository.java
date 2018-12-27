fd02cd7a2a95f4a762d97114fb6955bef6e2ae5e
Gyumin Kim <starkying@gmail.com>
2018-12-27 15:43:37 +0900

    Optimize imports and apply google style formatter

--- holidaysomething/src/main/java/com/holidaysomething/holidaysomething/repository/OrderRepository.java
+++ holidaysomething/src/main/java/com/holidaysomething/holidaysomething/repository/OrderRepository.java
@@ -1,13 +1,7 @@
 package com.holidaysomething.holidaysomething.repository;
 
-import com.holidaysomething.holidaysomething.domain.Member;
 import com.holidaysomething.holidaysomething.domain.Order;
-import java.time.LocalDateTime;
-import java.util.List;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
