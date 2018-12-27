98e204acc60e9798744c6c14f5b8890909d1cc9c
Jaeyong <koola97620@nate.com>
2018-12-27 16:55:19 +0900

    fix: memberRepository 수정

--- holidaysomething/src/main/java/com/holidaysomething/holidaysomething/controller/admin/member/AdminMemberController.java
+++ holidaysomething/src/main/java/com/holidaysomething/holidaysomething/controller/admin/member/AdminMemberController.java
@@ -2,18 +2,28 @@ package com.holidaysomething.holidaysomething.controller.admin.member;
 
 
 import com.holidaysomething.holidaysomething.domain.Member;
+import com.holidaysomething.holidaysomething.dto.OrderMemberDto;
 import com.holidaysomething.holidaysomething.dto.Search;
+import com.holidaysomething.holidaysomething.dto.SearchOrderMember;
 import com.holidaysomething.holidaysomething.service.MemberService;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/admin/member")
@@ -33,6 +43,33 @@ public class AdminMemberController {
   public String memberOrderSearch() {
 
     return "admin/member/member_order";
+  }
+
+  @PostMapping("/order/search")
+  public String memberOrderSearchPost(
+      @ModelAttribute(value = "SearchOrderMember") SearchOrderMember searchOrderMember,
+      @RequestParam(value = "date1") @DateTimeFormat(pattern = "MMddyyyy") StringBuffer date1,
+      @RequestParam(value = "date1") @DateTimeFormat(iso = ISO.DATE) StringBuffer date2) {
+
+    System.out.println(searchOrderMember.getName());
+    System.out.println(searchOrderMember.getLoginId());
+    System.out.println(searchOrderMember.getProductName());
+    System.out.println(searchOrderMember.getProductCode());
+    System.out.println(date1);
+    System.out.println(date2);
+    date1.append("T00:00:00");
+    date2.append("T23:59:59");
+    System.out.println(date1);
+    System.out.println(date2);
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    searchOrderMember.setOrderStartDate(LocalDateTime.parse(date1));
+    searchOrderMember.setOrderEndDate(LocalDateTime.parse(date2));
+
+    System.out.println(searchOrderMember.getOrderStartDate());
+    System.out.println(searchOrderMember.getOrderEndDate());
+
+    return "redirect:/admin/member/order/search";
   }
 
 
--- holidaysomething/src/main/java/com/holidaysomething/holidaysomething/service/ProductService.java
+++ holidaysomething/src/main/java/com/holidaysomething/holidaysomething/service/ProductService.java
@@ -10,6 +10,7 @@ import com.holidaysomething.holidaysomething.dto.Search;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public interface ProductService {
