2ad3fd38d3ab1f468b39f422de429e62ed7ee6aa
KimJunHyeong <jhkim4685@gmail.com>
2018-12-31 17:07:57 +0900

    error

--- holidaysomething/src/main/java/com/holidaysomething/holidaysomething/dto/productOptionDto.java
+++ holidaysomething/src/main/java/com/holidaysomething/holidaysomething/dto/productOptionDto.java
@@ -2,9 +2,9 @@ package com.holidaysomething.holidaysomething.dto;
 
 import lombok.Data;
 
-
 @Data
 public class ProductOptionDto {
+
   private Long id;
   private String name;
   private String description;
