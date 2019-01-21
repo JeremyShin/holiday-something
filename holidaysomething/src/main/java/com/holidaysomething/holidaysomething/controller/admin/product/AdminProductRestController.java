8357dca269f2475ae67dfb865275269ca0a48da0
KimJunHyeong <jhkim4685@gmail.com>
2019-01-21 21:22:12 +0900

    Fix : Tui Editor를 이용한 상품 등록 구현, 버그는 아직 못 잡음...
    @PostMapping("/add") Controller 사용 안 함, 유효성 검사 구현 필요, ajax Success가 실행하지 않음

--- holidaysomething/src/main/java/com/holidaysomething/holidaysomething/controller/admin/product/AdminProductRestController.java
+++ holidaysomething/src/main/java/com/holidaysomething/holidaysomething/controller/admin/product/AdminProductRestController.java
@@ -1,8 +1,10 @@
 package com.holidaysomething.holidaysomething.controller.admin.product;
 
+import com.holidaysomething.holidaysomething.domain.Product;
 import com.holidaysomething.holidaysomething.domain.ProductCategory;
 import com.holidaysomething.holidaysomething.domain.ProductImage;
 import com.holidaysomething.holidaysomething.domain.ProductOption;
+import com.holidaysomething.holidaysomething.dto.ProductAddDto;
 import com.holidaysomething.holidaysomething.dto.ProductOptionDto;
 import com.holidaysomething.holidaysomething.service.fileupload.ImageStreamService;
 import com.holidaysomething.holidaysomething.service.product.ProductAddService;
@@ -12,15 +14,22 @@ import com.holidaysomething.holidaysomething.service.product.ProductOptionServic
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.holidaysomething.holidaysomething.service.product.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/admin/product")
@@ -30,7 +39,6 @@ public class AdminProductRestController {
 
   private final ProductOptionService productOptionService;
   private final ProductAddService productAddService;
-  private final ProductImageService productImageService;
   private final ImageStreamService imageStreamService;
 
   /*
@@ -85,5 +93,35 @@ public class AdminProductRestController {
     String saveFileName = imageStreamService.save(multipartFile);
 
     return "/admin/product/image-files/" + saveFileName;
+  }
+
+  @PostMapping("/add/api")
+  public ResponseEntity productAddPost(
+          @Valid @RequestBody ProductAddDto productAddDto,
+          BindingResult bindingResult, ModelMap model) {
+
+    if (bindingResult.hasErrors()) {
+      for (ObjectError error : bindingResult.getAllErrors()) {
+        log.info(error.getDefaultMessage());
+        //model.addAttribute("product", product);
+      }
+//      return "admin/product/add";
+    } else {
+      //등록작업
+
+      System.out.println("등록작업을 시작합니다");
+      System.out.println("등록작업을 시작합니다");
+      System.out.println("등록작업을 시작합니다");
+      System.out.println("등록작업을 시작합니다");
+      System.out.println("등록작업을 시작합니다");
+      System.out.println("등록작업을 시작합니다z");
+
+      productAddDto.setRegDate(LocalDateTime.now());
+
+      productAddService.productRegister(productAddDto);
+
+
+    }
+    return new ResponseEntity<>(HttpStatus.OK);
   }
 }
