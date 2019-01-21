a7db0182244510765301b133dde40a29072b6a18
Jaeyong <koola97620@nate.com>
2019-01-21 12:46:41 +0900

    fix: 커스텀 에러 페이지 비활성화.

--- holidaysomething/src/main/java/com/holidaysomething/holidaysomething/config/ErrorConfig.java
+++ holidaysomething/src/main/java/com/holidaysomething/holidaysomething/config/ErrorConfig.java
@@ -11,9 +11,15 @@ import org.springframework.http.HttpStatus;
  * @author choijaeyong on 20/01/2019.
  * @project holidaysomething
  * @description
+ *
+ * 사이트 에러 발생시 톰캣 에러 페이지나 Whitelabel 페이지를 안 보이게 하고.
+ * 커스텀 페이지를 보이게 만드는 설정파일.
+ * 개발시엔 에러페이지가 보이는게 나을 것 같아서 일단 기능은 막아놓은 상태!
+ * 활성화 방법 : @Configuration 주석 제거
+ * MyErrorController 의 @Controller 주석 제거.
  */
 
-@Configuration
+//@Configuration
 public class ErrorConfig {
 
   @Bean
