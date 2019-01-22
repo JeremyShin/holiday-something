0ef3b21fe665a91e79761c2a3a11a8c37dc5e856
MisunJoo <0987someday@naver.com>
2019-01-22 11:51:49 +0900

    Add : 옵션등록만 js 경로 /static 붙임,  Error : 백오피스 css가 깨지는 문제 발견

--- holidaysomething/src/main/java/com/holidaysomething/holidaysomething/controller/user/order/UserOrderController.java
+++ holidaysomething/src/main/java/com/holidaysomething/holidaysomething/controller/user/order/UserOrderController.java
@@ -15,10 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- *
- */
-
 @Controller
 @RequestMapping("/user/order")
 @Slf4j
