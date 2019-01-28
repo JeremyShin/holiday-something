551ba7eb49ca590b0246498554d0b058d1ba5ad9
Jaeyong <koola97620@nate.com>
2019-01-28 16:04:29 +0900

    add: Security User 객체에 Member 참조변수 추가. user 상품 리스트 화면의 배너에 문자열 추가하는 방법 추가.

---
+++ holidaysomething/src/main/resources/static/js/user-product-list.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+  let categoryId = document.getElementById("hiddenCategoryId").value;
+
+  // 카테고리 id에 따라 banner-title, banner-description 에 다르게 내용을 채워주면 된다.
+  console.log("categoryId : " + categoryId);
+  if (categoryId == 4) {
+    let bannerTitleDiv = document.getElementById("banner-title");
+    let bannerDescriptionDiv = document.getElementById("banner-description");
+
+    let span1 = document.createElement('span');
+    let span1Text = document.createTextNode('textNode Title 야!!!!');
+
+    span1.appendChild(span1Text);
+    bannerTitleDiv.appendChild(span1);
+
+    let span2 = document.createElement('span');
+    let span2Text = document.createTextNode('textNode Description야!!!!');
+
+    span2.appendChild(span2Text);
+    bannerDescriptionDiv.appendChild(span2Text);
+
+    bannerTitleDiv.value = "It's FOOD Section!";
+    bannerDescriptionDiv.value = "각종 육류, 어류 그리고 디저트까지! 없는게 없는! Holiday Something!";
+  }
+
+}
