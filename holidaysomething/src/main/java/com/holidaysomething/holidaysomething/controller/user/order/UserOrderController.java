346c745c767e6c0d3a964c259f95428935bbac2a
MisunJoo <0987someday@naver.com>
2019-01-29 17:15:02 +0900

    Add : 상품상세페이지에서 주문페이지로 선택한 옵션리스트를 가져올 수 있음

--- holidaysomething/src/main/java/com/holidaysomething/holidaysomething/controller/user/order/UserOrderController.java
+++ holidaysomething/src/main/java/com/holidaysomething/holidaysomething/controller/user/order/UserOrderController.java
@@ -11,6 +11,7 @@ import com.holidaysomething.holidaysomething.service.member.MemberService;
 import com.holidaysomething.holidaysomething.service.order.OrderService;
 import com.holidaysomething.holidaysomething.service.product.ProductOrderService;
 import java.util.List;
+import javax.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -24,7 +25,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/user/order")
+@RequestMapping("/user/product/order")
 @Slf4j
 @RequiredArgsConstructor
 public class UserOrderController {
@@ -58,32 +59,13 @@ public class UserOrderController {
    * PostMapping으로 하는 이유는, GetMapping으로 받아오면 URL에 너무 많은 정보가 표시되며, 길이에 제한도 있기 때문임
    */
 
-//  @PostMapping
-//  public String orderPost(Model model,
-//      @AuthenticationPrincipal MemberUserDetails userDetails,
-//      ProductOrderInfoCommand poc){
-//
-//    AddOrderMemberDto addOrderMemberDto = memberService.findMemberById(userDetails.getId());
-//    model.addAttribute("addOrderMemberDto",addOrderMemberDto);
-//
-//    // ProductOrderInfoCommand 를 ProductOrderInfo의 리스트로 바꾸어주는 메소드
-//    List<ProductOrderInfoDto> productOrderInfoDtos = productOrderService.fromProductOrderInfoCommandToProductOrderInfoList(poc);
-//
-//    for(ProductOrderInfoDto productOrderInfoDto : productOrderInfoDtos){
-//      log.info(productOrderInfoDto.getProductId().toString());
-//      log.info(Integer.toString(productOrderInfoDto.getOptionIds().size()));
-//      log.info(Integer.toString(productOrderInfoDto.getOrderQuantities().size()));
-//    }
-//
-//    //model.addAttribute("productOrderInfoDto", productOrderInfoDto);
-//
-//    return "/user/order";
-//
-//  }
-
   @PostMapping
   public String orderPost(Model model,
+      @AuthenticationPrincipal MemberUserDetails userDetails,
       ProductOrderInfoCommand poc){
+
+    AddOrderMemberDto addOrderMemberDto = memberService.findMemberById(userDetails.getId());
+    model.addAttribute("addOrderMemberDto",addOrderMemberDto);
 
     // ProductOrderInfoCommand 를 ProductOrderInfo의 리스트로 바꾸어주는 메소드
     List<ProductOrderInfoDto> productOrderInfoDtos = productOrderService.fromProductOrderInfoCommandToProductOrderInfoList(poc);
@@ -96,10 +78,54 @@ public class UserOrderController {
 
     //model.addAttribute("productOrderInfoDto", productOrderInfoDto);
 
-    return "/user/order";
+    return "user/order";
 
   }
 
+//  @PostMapping
+//  public String orderPost(Model model,
+//      ProductOrderInfoCommand poc){
+//
+//    // ProductOrderInfoCommand 를 ProductOrderInfo의 리스트로 바꾸어주는 메소드
+//    List<ProductOrderInfoDto> productOrderInfoDto = productOrderService.fromProductOrderInfoCommandToProductOrderInfoList(poc);
+//
+//    log.info(Integer.toString(productOrderInfoDto.size()));
+//
+//    log.info("주문페이지로 넘어왔나?");
+//    log.info(productOrderInfoDto.get(0).getProductId().toString());
+//    log.info(productOrderInfoDto.get(0).getOptionIds().toString());
+//    log.info(productOrderInfoDto.get(0).getOrderQuantities().toString());
+//
+//    log.info(Integer.toString(productOrderInfoDto.get(0).getOptionIds().size()));
+//    log.info(Integer.toString(productOrderInfoDto.get(0).getOrderQuantities().size()));
+//
+//
+//    //model.addAttribute("productOrderInfoDto", productOrderInfoDto);
+//
+//    return "user/order";
+//
+//  }
+
+
+//  @PostMapping
+//  public String orderPost(Model model,
+//      ProductOrderInfoDto productOrderInfoDto){
+//
+//    // ProductOrderInfoCommand 를 ProductOrderInfo의 리스트로 바꾸어주는 메소드
+//
+//    log.info("주문페이지로 넘어왔나?");
+//      log.info(productOrderInfoDto.getProductId().toString());
+//      log.info(productOrderInfoDto.getOptionIds().toString());
+//      log.info(productOrderInfoDto.getOrderQuantities().toString());
+//
+//      log.info(Integer.toString(productOrderInfoDto.getOptionIds().size()));
+//      log.info(Integer.toString(productOrderInfoDto.getOrderQuantities().size()));
+//
+//    //model.addAttribute("productOrderInfoDto", productOrderInfoDto);
+//
+//    return "user/order";
+//
+//  }
 
 
 
