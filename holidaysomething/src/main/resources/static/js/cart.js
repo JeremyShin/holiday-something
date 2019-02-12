4612ef373edbd11c5922431d529816cf24527930
Gyumin Kim <starkying@gmail.com>
2019-02-12 12:09:33 +0900

    카트 상품수량 변경버튼 / CONTINUE SHOPPING 버튼

--- holidaysomething/src/main/resources/static/js/cart.js
+++ holidaysomething/src/main/resources/static/js/cart.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+
   let removeBtns = document.querySelectorAll('.btn-remove');
   removeBtns.forEach((button) => {
     button.addEventListener('click', (e) => {
@@ -14,4 +15,28 @@ document.addEventListener('DOMContentLoaded', () => {
       // .catch(err => console.error(err));
     });
   });
+
+  // CONTINUE SHOPPING 버튼 클릭 => 메인페이지로 이동
+  let continueShopppingBtn = document.querySelector('.btn-shopping');
+  continueShopppingBtn.addEventListener('click', () => {
+    window.location.href = '/';
+  });
+
+  // 카트 페이지에서 각 상품 수량 '-' / '+' 버튼 클릭 시 수량 변경
+  let quantityMinusBtn = document.querySelectorAll('.quantity-minus');
+  quantityMinusBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      let plusBtn = btn.nextElementSibling;
+      if (plusBtn.value > 0)
+        plusBtn.setAttribute('value', parseInt(plusBtn.value) - 1);
+    });
+  });
+  let quantityPlusBtn = document.querySelectorAll('.quantity-plus');
+  quantityPlusBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      let minusBtn = btn.previousElementSibling;
+      minusBtn.setAttribute('value', parseInt(minusBtn.value) + 1);
+    });
+  });
+
 });
