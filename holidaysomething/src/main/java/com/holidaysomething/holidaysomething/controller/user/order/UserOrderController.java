272ce55255ac48f63a3d18af9e4a71d0f08c1214
MisunJoo <0987someday@naver.com>
2019-02-14 16:02:03 +0900

    Add : 주문완료페이지 필요한 정보들 모두 받아옴 ( css는 미구현)

--- holidaysomething/src/main/java/com/holidaysomething/holidaysomething/controller/user/order/UserOrderController.java
+++ holidaysomething/src/main/java/com/holidaysomething/holidaysomething/controller/user/order/UserOrderController.java
@@ -6,6 +6,7 @@ import com.holidaysomething.holidaysomething.domain.OrderedProduct;
 import com.holidaysomething.holidaysomething.domain.Product;
 import com.holidaysomething.holidaysomething.domain.ProductOption;
 import com.holidaysomething.holidaysomething.domain.Shipping;
+import com.holidaysomething.holidaysomething.domain.constant.ShippingStatus;
 import com.holidaysomething.holidaysomething.dto.AddOrderMemberDto;
 import com.holidaysomething.holidaysomething.dto.ProductDetailDto;
 import com.holidaysomething.holidaysomething.dto.ProductOptionCommand;
@@ -52,24 +53,6 @@ public class UserOrderController {
   private final ProductService productService;
   private final ShippingService shippingService;
   private final OrderedProductService orderedProductService;
-
-  //어떤 회원이 주문했는지 받아오기, 어떤 상품을 주문했는지 받아오기
-//  @GetMapping
-//  public String order(Model model,
-//      @AuthenticationPrincipal MemberUserDetails userDetails,
-//      ProductOrderInfoCommand poc){
-//
-//    AddOrderMemberDto addOrderMemberDto = memberService.findMemberById(userDetails.getId());
-//    model.addAttribute("addOrderMemberDto",addOrderMemberDto);
-//
-//    // ProductOrderInfoCommand 를 ProductOrderInfo의 리스트로 바꾸어주는 메소드
-//    List<ProductOrderInfoDto> productOrderInfoDtos = productOrderService.fromProductOrderInfoCommandToProductOrderInfoList(poc);
-//
-//
-//    //model.addAttribute("productOrderInfoDto", productOrderInfoDto);
-//
-//    return "user/order";
-//  }
 
   //주문 버튼을 누르면, orderd_product, order, shipping, payment에 데이터가 추가됨
 
@@ -142,9 +125,23 @@ public class UserOrderController {
     // 배송테이블 등록
     ShippingDto shippingResult = shippingService.addShipping(shippingDto);
 
+    List<ProductOrderDetailDto> productOrderDetailDtos = new ArrayList<>();
+
+    for (OrderedProduct orderedProduct : orderedProducts) {
+      ProductOrderDetailDto productOrderDetailDto =
+          productService.getProductForOrder(orderedProduct.getProduct().getId(),
+              orderedProduct.getProductOption().getId(), orderedProduct.getQuantity());
+      productOrderDetailDtos.add(productOrderDetailDto);
+    }
+
+    String orderStatus = ShippingStatus.getValuesByKey(order.getStatus());
+    log.info("주문상태는" + orderStatus);
+
     model.addAttribute("order", order);
-    model.addAttribute("orderedProducts", orderedProducts);
+    model.addAttribute("productOrderDetailDtos", productOrderDetailDtos);
     model.addAttribute("shipping",shippingResult);
+    model.addAttribute("orderStatus", orderStatus);
+    model.addAttribute("productOrderCompleteDto", productOrderCompleteDto);
 
     return "user/order-complete";
   }
